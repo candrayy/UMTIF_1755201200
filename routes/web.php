7cd592582fc4e7c2702fc781e92d4ba4e73fbf38
candrayy <ahmad.candra.17@gmail.com>
2020-07-31 20:54:26 +0700

    Tambah View Home

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('foo', function () {
-    return 'Hello World';
+Route::get('Home', function () {
+    return view('Home');
 });
